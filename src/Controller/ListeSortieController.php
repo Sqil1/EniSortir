6426b5e2f18162193253f1b962e1cef745d1bc24
Sqil1 <mbadoian@gmail.com>
2023-11-17 19:50:12 +0100

    réfection du formulaire avec filtre nom, campus, date et organisateur

--- src/Controller/ListeSortieController.php
+++ src/Controller/ListeSortieController.php
@@ -21,19 +21,12 @@ class ListeSortieController extends AbstractController
         $form = $this->createForm(SearchForm::class, $data);
         $form->handleRequest($request);
 
-
-
-        if(isset($data->isOrganisateur) && $data->isOrganisateur) {
-            // Récupérez l'utilisateur connecté
+        if (isset($data->isOrganisateur) && $data->isOrganisateur) {
             $currentUser = $security->getUser();
-            // Vérifiez si l'utilisateur est connecté et si oui, récupérez son ID
             $organisateurId = $currentUser ? $currentUser->getId() : null;
-            // Définissez la propriété dans l'objet SearchData
             $data->organisateur = $organisateurId;
         }
 
-
-        // Utilisez l'ID de l'organisateur pour filtrer les sorties
         $sorties = $sortieRepository->findSearch($data);
         //dd($data);
         return $this->render('sortie/liste.html.twig', [
@@ -41,51 +34,4 @@ class ListeSortieController extends AbstractController
             'form' => $form->createView()
         ]);
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//    public function liste(Request $request, SortieRepository $sortieRepository): Response
-//    {
-//        $sortie = new Sortie();
-//        $sortiesForm = $this->createForm(ListeSortiesType::class, $sortie);
-//        $sortiesForm->handleRequest($request);
-//        $user = $this->getUser();
-//
-//
-//        if ($sortiesForm->isSubmitted() && $sortiesForm->isValid()) {
-//            $formData = $sortiesForm->getData();
-//
-//            $sorties = $sortieRepository->findByCritere(
-//                [
-//                'nom' => $formData->getNom(),
-//                'campus' => $formData->getCampus(),
-//                'dateHeureDebut' => $formData->getDateHeureDebut(),
-//                ],
-//            $formData->getIsOrganisateur()
-//        );
-//
-//            //dd($formData);
-//    } else {
-//            $sorties = $sortieRepository->findAll();
-//        }
-//
-//        $filtreCampus = $request->query->get('campus');
-//
-//        return $this->render('sortie/liste.html.twig', [
-//            "sorties" => $sorties,
-//            "filtreCampus" => $filtreCampus,
-//            'sortieForm' => $sortiesForm->createView(),
-//        ]);
-//    }
 }
