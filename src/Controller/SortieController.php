b00c02971cb8c7fe00666dab9cacbd949093a347
Sqil1 <mbadoian@gmail.com>
2023-11-20 11:03:21 +0100

    ajout des filtres en sorties auquelles je suis isncriet/non inscrit, ajout du dernier filtre par sortie passées (cloturées). ajout de l'affichage si l'utilsateur est inscrit.

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -57,4 +57,69 @@ class SortieController extends AbstractController
             'sortieForm' => $sortieForm->createView()
         ]);
     }
+
+    #[Route('/show/{id}', name: 'detail')]
+    public function show(int $id, SortieRepository $sortieRepository): Response
+    {
+        $sortie = $sortieRepository->find($id);
+        $participant = $this->getUser();
+
+        return $this->render('sortie/show.html.twig', [
+            'sortie' => $sortie,
+            'participant' => $participant,
+        ]);
+    }
+
+    #[Route('/inscription/{id}', name: 'inscription')]
+    public function inscription(Sortie $sortie, EntityManagerInterface $manager, EtatRepository $etat): RedirectResponse
+    {
+        $participant = $this->getUser();
+        if (!$participant) {
+            return $this->redirectToRoute('app_login');
+        }
+
+        $etatOuverte = $etat->findOneBy(['libelle' => 'Ouverte']);
+        if (
+            !$etatOuverte ||
+            $sortie->getEtat() !== $etatOuverte ||
+            new \DateTime() > $sortie->getDateLimiteInscription() ||
+            $sortie->getParticipants()->count() >= $sortie->getNbInscriptionsMax()
+        ) {
+            return $this->redirectToRoute('home');
+        }
+
+        $sortie->addParticipant($participant);
+
+        $manager->flush();
+        return $this->redirectToRoute('sortie_detail', ['id' => $sortie->getId()]);
+    }
+
+    #[Route('/desistement/{id}', name: 'desistement')]
+    public function desister(Sortie $sortie, EntityManagerInterface $manager): Response
+    {
+        $participant = $this->getUser();
+
+        $now = new \DateTime();
+
+        if (
+            $sortie->getDateHeureDebut() > $now &&
+            $sortie->getDateLimiteInscription() > $now
+        ) {
+
+            $participant = $this->getUser();
+            $sortie->removeParticipant($participant);
+
+            $manager->flush();
+
+            return $this->redirectToRoute('sortie_detail', ['id' => $sortie->getId()]);
+        } else {
+
+            $this->addFlash(
+                'warning',
+                'Vous ne pouvez pas vous désister de cette sortie.'
+            );
+
+            return $this->redirectToRoute('sortie_detail', ['id' => $sortie->getId()]);
+        }
+    }
 }
