3dac7e75ee658294dccda4c7f319e9015dfca672
JordanRobin <jordanrobin100@gmail.com>
2023-11-21 16:40:14 +0100

    dernières retouches formulaire création sortie

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -6,6 +6,7 @@ use App\Entity\Participant;
 use App\Entity\Sortie;
 use App\Form\SortieType;
 use App\Repository\EtatRepository;
+use App\Repository\LieuRepository;
 use App\Repository\ParticipantRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -22,31 +23,37 @@ class SortieController extends AbstractController
                             ParticipantRepository $participantRepository,
                             EtatRepository $etatRepository): Response
     {
-        /*récupération d'un participant et un campus - A RECUPERER AVEC LA SESSION*/
+        /*Récupération d'un participant et un campus - A RECUPERER AVEC LA SESSION*/
+        //$organisateur = $this->getUser();
         $organisateur = $participantRepository->find(1);
 
-        /*l'état est 'Créée' si validation avec 'Enregistrer', sinon il est 'Ouverte'*/
-        //decaler la récupération de l'état après handlerequest ?
-        $etat = $etatRepository->find(1);
-
         $sortie = new Sortie();
-        $sortie->setEtat($etat);
         $sortie->setOrganisateur($organisateur);
         $sortie->setCampus($organisateur->getCampus());
 
         $sortieForm = $this->createForm( SortieType::class, $sortie );
-
         $sortieForm->get('campus')->setData($organisateur->getCampus()->getNom() );
-        $sortieForm->get('dateLimiteInscription')->setData( new \DateTimeImmutable('+2 days') );
-        /*MARCHE PAS :
-        $sortieForm->setData([
-            'campus' => $organisateur->getCampus()->getNom(),
-            'dateLimiteInscription' => new \DateTimeImmutable('+2 days')
-        ]);*/
+        $sortieForm->get('dateHeureDebut')->setData( new \DateTimeImmutable('+2 days') );
+        $sortieForm->get('dateLimiteInscription')->setData( new \DateTimeImmutable('+1 days') );
 
         $sortieForm->handleRequest($request);
 
         if ( $sortieForm->isSubmitted() && $sortieForm->isValid() ) {
+
+            //L'état est 'Créée' si validation avec 'Enregistrer', sinon il est 'Ouverte'
+            $etatCréée = $etatRepository->findOneBy([ "libelle" => "Créée" ]);
+            $etatOuverte = $etatRepository->findOneBy([ "libelle" => "Ouverte" ]);
+            if ( $sortieForm->get('enregistrer')->isClicked() ) {
+                $sortie->setEtat($etatCréée);
+            } elseif ( $sortieForm->get('publier')->isClicked() ) {
+                $sortie->setEtat($etatOuverte);
+            } else {
+                return $this->render('sortie/create.html.twig', [
+                    'sortieForm' => $sortieForm->createView()
+                ]);
+            }
+
+            //Sauvegarde de la sortie
             $entityManager->persist($sortie);
             $entityManager->flush();
             $this->addFlash( 'success', 'La sortie a bien été ajoutée !' );
@@ -57,4 +64,23 @@ class SortieController extends AbstractController
             'sortieForm' => $sortieForm->createView()
         ]);
     }
+
+
+    //utilisée par une requête ajax dans create.html.twig
+    #[Route( '/updateLieu', name: 'updateLieu', methods: ['GET'] )]
+    public function affichageLieu( Request $request, LieuRepository $lieuRepository ) : Response {
+
+        $id = $request->query->get('sortie_lieu');
+        $lieuChoisi = $lieuRepository->find($id);
+        $villeCorrespondante = $lieuChoisi->getVille();
+
+        return $this->json([
+            'rue' => $lieuChoisi->getRue(),
+            'codePostal' => $villeCorrespondante->getCodePostal(),
+            'latitude' => $lieuChoisi->getLatitude(),
+            'longitude' => $lieuChoisi->getLongitude()
+        ]);
+
+    }
+
 }
